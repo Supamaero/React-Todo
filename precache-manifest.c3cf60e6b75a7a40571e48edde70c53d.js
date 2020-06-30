1955068043fa21b1c3e3f0bba996dab927890502
Supamaero <muwede6@gmail.com>
2020-06-30 11:20:11 +0100

    Updates

---
+++ precache-manifest.c3cf60e6b75a7a40571e48edde70c53d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6f2335f79e3b68b9e25ca0ae3afbe6",
+    "url": "/Supamaero/React-Todo.git/index.html"
+  },
+  {
+    "revision": "77d6e605081e7008dc0e",
+    "url": "/Supamaero/React-Todo.git/static/css/main.405f1586.chunk.css"
+  },
+  {
+    "revision": "c749d437792d7a13aa7e",
+    "url": "/Supamaero/React-Todo.git/static/js/2.230742e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Supamaero/React-Todo.git/static/js/2.230742e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d6e605081e7008dc0e",
+    "url": "/Supamaero/React-Todo.git/static/js/main.03ee0c93.chunk.js"
+  },
+  {
+    "revision": "641f8f44af6aad0a6bc1",
+    "url": "/Supamaero/React-Todo.git/static/js/runtime-main.37a0516e.js"
+  },
+  {
+    "revision": "966e5d311721e82499d16b64e4f26b63",
+    "url": "/Supamaero/React-Todo.git/static/media/home.966e5d31.svg"
+  },
+  {
+    "revision": "99d58fcae24f7cd1fd75bc47002fd265",
+    "url": "/Supamaero/React-Todo.git/static/media/trash.99d58fca.svg"
+  }
+]);
